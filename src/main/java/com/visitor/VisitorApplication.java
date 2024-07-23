2f2ccea73c95761dc257816000f26bd55e1c06e8
aavritti <aavritti.soni1996@gmail.com>
2024-07-24 09:32:10 +1000

    Chnage for main class

--- src/main/java/com/visitor/VisitorApplication.java
+++ src/main/java/com/visitor/VisitorApplication.java
@@ -6,37 +6,33 @@ import com.visitor.models.Triangle;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 
 public class VisitorApplication {
 
     public static void main(String[] args) {
+        final List<Shape> shapes = new ArrayList<>();
 
-        Map<String, Integer> mp = new HashMap<String, Integer>();
-        mp.put("A", 14);
-        mp.put("B", 2);
-        mp.put("C", 2);
-        mp.put("D", 4);
+        shapes.add(new Circle(10));
+        shapes.add(new Rectangle(10, 2));
 
-        Map.Entry<String, Integer> collect = mp.entrySet().stream().sorted(Map.Entry.comparingByValue()).
-                collect(Collectors.toList()).get(2);
+        final AreaVisitor areaVisitor = new AreaVisitor();
 
-        List<Map.Entry<String, Integer>> collect1 = mp.entrySet().stream().sorted(Collections
-                .reverseOrder(Map.Entry.comparingByValue())).collect(Collectors.toList());
+        for (Shape shape: shapes) {
+            shape.accept(areaVisitor);
+            final double area = areaVisitor.get();
 
-        List<Map.Entry<Integer, List<Map.Entry<String, Integer>>>> collect2 =
-                mp.entrySet().stream().collect(Collectors.groupingBy(Map.Entry::getValue)).entrySet()
-                        .stream().collect(Collectors.toList());
+            System.out.printf(
+                    "Area of %s: %.2f%n",
+                    shape.getClass().getSimpleName(),
+                    area
+            );
+        }
 
-        Map.Entry<Integer, List<String>> integerListEntry = mp.entrySet().stream().collect(Collectors.groupingBy(Map.Entry::getValue,
-                        Collectors.mapping(Map.Entry::getKey, Collectors.toList()))).entrySet().stream().sorted(Collections.reverseOrder(
-                                Map.Entry.comparingByKey()))
-                .collect(Collectors.toList()).get(2);
-        System.out.println(integerListEntry);
+        System.out.println("---------------------------------");
+
+
     }
 }
